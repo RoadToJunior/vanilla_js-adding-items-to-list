58a273694c67e64c4f36ac1a9eb4a4aed6aea0d4
NowWeAre-Free <kontodarm@wp.pl>
2023-08-09 15:41:29 +0200

    add searcher

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 const listItems = document.getElementsByClassName("task");
 const taskNumber = document.querySelector("h1 span");
+const searchInput = document.getElementById("search");
 
 const addTask = (e) => {
   e.preventDefault();
@@ -12,6 +13,7 @@ const addTask = (e) => {
   task.className = "task";
   task.innerHTML = `${titleTask} <button>X</button>`;
   ul.appendChild(task);
+  input.value = "";
   taskNumber.textContent = listItems.length;
   task.addEventListener("click", removeTask);
 };
@@ -22,4 +24,15 @@ const removeTask = (e) => {
   taskNumber.textContent = listItems.length;
 };
 
+const checkIf = (e) => {
+  const searchText = e.target.value.toLowerCase();
+  let tasks = [...listItems];
+  tasks = tasks.filter((li) =>
+    li.textContent.toLowerCase().includes(searchText)
+  );
+  ul.textContent = "";
+  tasks.forEach((li) => ul.appendChild(li));
+};
+
+searchInput.addEventListener("input", checkIf);
 form.addEventListener("submit", addTask);
